306e60b5d7e96e4bcd4f930136dd30dd46791d97
aman_tekion <amankm@tekion.com>
2022-10-07 11:09:14 +0530

    first commit

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   Alert,
@@ -15,9 +15,12 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
+  TextInput
 } from 'react-native';
+import PushNotification from 'react-native-push-notification';
 
 import {
   Colors,
@@ -29,96 +32,96 @@ import {
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+
+
+const createChannel=()=>{
+  PushNotification.createChannel(
+    {
+      channelId: "channel-test", // (required)
+      channelName: "test channel", // (required)
+  })
+
+}
 
 const App= () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
+  const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   const onPressHeader=()=>{
-    Alert.alert('alert')
+   // Alert.alert('warning!')
+   PushNotification.localNotification({
+    channelId:'channel-test',
+    title:'warning',
+    message:'warning sdwqdqwdqwd ',
+  })
+
+  // PushNotification.scheduleLocalNotification({
+  //   channelId:'channel-test',
+  //   title:'warning',
+  //   message:'warningwarningwarningwarning warningwarning  warningwarningwarning  warningwarning',
+  //   date: new Date(Date.now() + 5 * 1000),
+  //   allowWhileIdle:true,
+  // })
+
   }
+
+  useEffect(()=>{
+    createChannel();
+  },[])
 
   return (
 
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView >
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        
-        <Header/>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+        <View style={styles.searchSection}>
+      {/* <Icon style={styles.searchIcon} name="ios-search" size={20} color="#000"/> */}
+     
+      <TextInput
+          style={styles.input}
+          placeholder="User Nickname"
+          onChangeText={(searchString) => {this.setState({searchString})}}
+          underlineColorAndroid="transparent"
+      />
+       <Text style={styles.searchIcon}>H</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  searchSection: {
+
+    marginTop:50,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+
+    borderColor:'red',
+    borderWidth:1,
+},
+searchIcon: {
+    padding: 10,
+    fontSize:10,
+    fontWeight:'800',
+
+},
+input: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#fff',
+    color: '#424242',
+},
 });
 
 export default App;
